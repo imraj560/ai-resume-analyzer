97a86ccdcbb2e747e2342b18739be0e9e9809b62
imraj560 <raju560.webdev@gmail.com>
2025-07-29 18:10:06 -0400

    Home Job Card Component Set

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,44 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resubot" },
+    { name: "description", content: "Taylor Your Career" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application and Resume Rating</h1>
+        <h2>Leverage Ai to Taylor Your CV</h2>
+      </div>
+    </section>
+
+    {
+      resumes.length > 0 && (
+          <div className="resumes-section">
+            {
+              resumes.map((resume) => {
+
+                return(
+
+                    <ResumeCard key={resume.id} resume={resume} />
+                )
+
+              })
+            }
+          </div>
+        )
+    }
+
+
+
+  </main>
 }
